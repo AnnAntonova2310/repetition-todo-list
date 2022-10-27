94b53be3521c10b8daa0c46b0818a62b3f41e1e1
AnnAntonova2310 <anyashaida@mail.ru>
2022-10-27 20:41:01 +0500

    Initial commit

---
+++ src/TodoList.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type todoListType={
+    title: string
+}
+
+export function TodoList(props: todoListType) {
+    return (
+        <div className="App">
+            <div>
+                <h3>{props.title}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    );
+}
