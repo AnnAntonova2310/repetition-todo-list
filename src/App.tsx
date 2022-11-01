ac1196bd9a941bb7fdcf43cca06f96ac2a033dbe
AnnAntonova2310 <anyashaida@mail.ru>
2022-11-01 12:39:48 +0500

    Map

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ const App=()=> {
     const tasks1 = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false}
-    ]
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest API", isDone: false},
+        {id: 5, title: "GraphQL", isDone: false},
+            ]
     const tasks2 = [
         {id: 1, title: "Hello world", isDone: true},
         {id: 2, title: "I am Happy", isDone: false},
